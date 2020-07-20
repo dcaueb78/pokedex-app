46518b1502d6f8eb97ec07dc7e2dc3223c0b4bd2
Cauê Kotarski <caue1017@hotmail.com>
2020-07-20 14:37:23 -0300

    chore: initial expo commit

---
+++ components/Icons/PokemonIcon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image } from 'react-native';
+import Colors from '../../constants/Colors';
+import { useEffect, useState } from 'react';
+
+interface Props {
+  color: String;
+}
+
+export const PokemonIcon: React.FC<Props> = ({ color }) => {
+  const activeIconPath = require('../../assets/images/pokemon_icon.png');
+  const inactiveIconPath = require('../../assets/images/pokemon_icon_inactive.png');
+  const [selectedIcon, setSelectedIcon] = useState<String>(activeIconPath);
+  
+  useEffect(()=> {
+    const oi = color === Colors.light.tint ? activeIconPath : inactiveIconPath;
+    setSelectedIcon(oi);
+  }, []);
+
+  console.log(color);
+
+  return (
+    <Image
+      source={selectedIcon}
+      fadeDuration={0}
+      style={{width: 30, height: 30}}
+    />
+  );
+}
---
+++ types.tsx
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Pokémon: undefined;
+  Moves: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
